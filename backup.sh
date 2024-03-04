4b9e9b5c2ef4c5231343d75d7a2be0f3ec862dde
Theneau Maxime <theneau.maxime@gmail.com>
2024-03-04 16:09:10 +0100

    Feat: deleted file

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # Chargement des variables d'environnement depuis le fichier .env.local
 source .env.local
@@ -8,6 +7,14 @@ CURRENT_DATE=$(date +%Y%m%d)
 # Création du répertoire de sauvegarde avec les bonnes permissions
 mkdir -p $BACKUP_DIR_BASE
 chown $USER:$USER $BACKUP_DIR_BASE
+
+# Supprimer les fichiers de sauvegarde datant de plus d'une semaine, sauf pour un fichier par jour de la semaine
+for file in $(find $BACKUP_DIR_BASE -type f -name "backup_*.sql.gz" -mtime +7); do
+    day=$(date -d "$(basename "$file" | cut -d'_' -f2 | cut -d'.' -f1)" "+%A")
+    if [ "$day" != "Monday" ]; then
+        rm "$file"
+    fi
+done
 
 # Création du répertoire de sauvegarde du jour
 BACKUP_DIR="$BACKUP_DIR_BASE/backup_$CURRENT_DATE"
