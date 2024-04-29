a130c19323c96091d191f173596784fc7ad87ffc
Theneau Maxime <theneau.maxime@gmail.com>
2024-04-29 17:16:39 +0200

    Feat: create backup daily

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
-
 # Chargement des variables d'environnement depuis le fichier .env.local
 source .env.local
 
@@ -6,7 +5,7 @@ CURRENT_DATE=$(date +%Y%m%d)
 
 # Création du répertoire de sauvegarde avec les bonnes permissions
 mkdir -p $BACKUP_DIR_BASE
-chown $USER:$USER $BACKUP_DIR_BASE
+chown $DB_USER:$DB_USER $BACKUP_DIR_BASE
 
 # Supprimer les fichiers de sauvegarde datant de plus d'une semaine, sauf pour un fichier par jour de la semaine
 for file in $(find $BACKUP_DIR_BASE -type f -name "backup_*.sql.gz" -mtime +7); do
@@ -16,10 +15,10 @@ for file in $(find $BACKUP_DIR_BASE -type f -name "backup_*.sql.gz" -mtime +7); 
     fi
 done
 
-# Création du répertoire de sauvegarde du jour
-BACKUP_DIR="$BACKUP_DIR_BASE/backup_$CURRENT_DATE"
-mkdir -p $BACKUP_DIR/mysql
+# Récupération de la liste des bases de données
+databases=$(mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|sys)")
 
-# Sauvegarde de la base de données
-mysqldump --all-databases | gzip > $BACKUP_DIR/mysql/backup_$CURRENT_DATE.sql.gz
-
+# # Sauvegarde de chaque base de données séparément
+for db in $databases; do
+    mysqldump --single-transaction -u "$DB_USER" -p"$DB_PASSWORD" "$db" | gzip > "$BACKUP_DIR_BASE/mysql/$db-$CURRENT_DATE.sql.gz"
+done
