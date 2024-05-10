eb4112bdfdd3a6219ea39caed7c4fb4f10334555
Theneau Maxime <theneau.maxime@gmail.com>
2024-05-10 14:07:50 +0200

    Feat: change backup.sh

--- backup.sh
+++ backup.sh
@@ -12,10 +12,8 @@ BACKUP_DIR_CLONE=$BACKUP_DIR/rsync
 SOURCE_DIR="/var /etc /home /usr/local /root" 
 
 # Création du répertoire de sauvegarde avec les bonnes permissions
-sudo mkdir -p "$BACKUP_DIR_CLONE"
-sudo chown "$DB_USER:$DB_USER" "$BACKUP_DIR_CLONE"
-sudo mkdir -p "$BACKUP_DIR_MYSQL"
-sudo chown "$DB_USER:$DB_USER" "$BACKUP_DIR_MYSQL"
+ mkdir -p "$BACKUP_DIR_CLONE"
+ mkdir -p "$BACKUP_DIR_MYSQL"
 
 
 # Récupération de la liste des bases de données
@@ -27,22 +25,18 @@ for db in $databases; do
 done
 
 # Récupération de la liste des répertoires
-excluded_directories="--exclude=/var/cache --exclude=/var/tmp --exclude=/var/backups --exclude=/var/lib/docker --exclude=$BACKUP_DIR_BASE"
+# excluded_directories="--exclude=/var/cache --exclude=/var/tmp --exclude=/var/backups --exclude=/var/lib/docker --exclude=$BACKUP_DIR_BASE"
 rsync -aAXv $excluded_directories  $SOURCE_DIR $BACKUP_DIR_CLONE
 
 # Compression de l'archive
 tar -czf "$BACKUP_DIR.tar.gz" -C "$BACKUP_DIR" .
 
-# Suppression des fichiers temporaires
-rm -rf "$BACKUP_DIR"
-
 # Supprimer les backups de plus de 3 jours
 rm -rf $BACKUP_DIR_BASE/$OLDER_DATE.tar.gz
-echo "Backup $OLDER_DATE terminé"
+rm -rf $BACKUP_DIR_BASE/$OLDER_DATE
+
 # AWS S3 
 aws s3 cp "$BACKUP_DIR.tar.gz" s3://$S3_BUCKET/$CURRENT_DATE.tar.gz
 
 # # Suppression de l'archive AWS S3 de plus de 3 jours
 aws s3 rm s3://$S3_BUCKET/$OLDER_DATE.tar.gz
-
-
